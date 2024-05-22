0bc4d258aea747f0f429a47614c2a5740a75f02a
Robbie Kooistra <robbiekooistra2000@gmail.com>
2024-05-22 22:26:12 +0200

    Regen hello world app properly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
-  template: `
-    <h1>Hello world</h1>
-  `,
-  styleUrls: ['./app.component.css'],
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'default';
+  title = 'ou_webapp';
 }
