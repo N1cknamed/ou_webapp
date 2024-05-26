451912c65bba954bfebdeb1f794a5bcbd6ca694e
Robbie Kooistra <robbiekooistra2000@gmail.com>
2024-05-26 21:12:16 +0200

    Implement basic routing and bootstrap navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { MapComponent } from './map/map.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, RouterModule, HomeComponent, MapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
