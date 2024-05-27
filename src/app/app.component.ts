4b5d172bda5674ebbcb60e922e022b437910e540
Robbie Kooistra <robbiekooistra2000@gmail.com>
2024-05-27 13:01:12 +0200

    Implement routing and forms for authentication system

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MapComponent } from './map/map.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AdminComponent } from './admin/admin.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, HomeComponent, MapComponent],
+  imports: [RouterOutlet, RouterModule, HomeComponent, MapComponent, LoginComponent, RegisterComponent, AdminComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MapComponent } from './map/map.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AdminComponent } from './admin/admin.component';
 
 export const routeConfig: Routes = [
     {
@@ -12,5 +15,20 @@ export const routeConfig: Routes = [
         path: 'map',
         component: MapComponent,
         title: 'Map'
+    },
+    {
+        path: 'login',
+        component: LoginComponent,
+        title: 'Login'
+    },
+    {
+        path: 'register',
+        component: RegisterComponent,
+        title: 'Register'
+    },
+    {
+        path: 'admin',
+        component: AdminComponent,
+        title: 'Admin'
     }
 ];
