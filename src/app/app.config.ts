59ee25f39399f6fe2918d82ffcab29c34909fe16
Robbie Kooistra <robbiekooistra2000@gmail.com>
2024-05-27 12:33:04 +0200

    Add PWA functionality

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routeConfig } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routeConfig), provideClientHydration()]
+  providers: [provideRouter(routeConfig), provideClientHydration(), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })]
 };
