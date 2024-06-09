1ed5f832661d3b1b20dd7ed6979039925cfd5d0e
Thomasa <60262650+JustThomasa@users.noreply.github.com>
2024-06-09 22:56:43 +0200

    Provided HttpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,8 +5,10 @@ import { routeConfig } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideServiceWorker } from '@angular/service-worker';
 
+import { provideHttpClient } from '@angular/common/http';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routeConfig), provideClientHydration(), provideServiceWorker('ngsw-worker.js', {
+  providers: [provideHttpClient(), provideRouter(routeConfig), provideClientHydration(), provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
         registrationStrategy: 'registerWhenStable:30000'
     })]
