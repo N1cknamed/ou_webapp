0a12e1e0087ceeb5a1031acc2074d1c3f16de991
Robbie Kooistra <robbiekooistra2000@gmail.com>
2024-06-10 03:18:40 +0200

    Implement first bits of broken login system

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,29 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const { username, password } = this.loginForm.value;
+      // Use HttpClient to send a login request to your backend
+      // this.http.post('/api/login', { username, password }).subscribe(/* handle response */);
+    }
+  }
 }
